994da4a09ee93d4d0ff78063da30e21cfda7eedc
llihn10 <nblinggg10@gmail.com>
2026-02-03 13:05:55 +0700

    fix backend connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,25 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
+const express = require('express')
+require("dotenv").config();
 
+const cors = require("cors");
+const connectDB = require("./dbConnect/db")
+const db = require("./models")
+const productRoute = require('./routers/product.route')
 
-app.get('/', async (req, res) => {
-    try {
-        res.send({ message: 'Welcome to Practical Exam!' });
-    } catch (error) {
-        res.send({ error: error.message });
-    }
+const app = express()
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to Restful API server' });
 });
-
+app.use('/', productRoute)
 
 const PORT = process.env.PORT || 9999;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}).catch(err => {
+    console.error("Cannot connect to database: ", err);
+    process.exit(1);
 });
